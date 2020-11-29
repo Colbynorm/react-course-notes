fe7127d287bef9c0e4bac4cb3345d774a5136dbc
Colby Norman <colbynorm41@gmail.com>
2020-11-29 11:11:29 -0600

    added classes notes

--- chapter-2-JS/javaScriptNotes.js
+++ chapter-2-JS/javaScriptNotes.js
@@ -15,3 +15,25 @@ const multiply = number => number * 2;
 console.log(multiply(2));
 
 //Classes
+class Human {
+    constructor() {
+        this.gender = "male";
+    }
+    printGender() {
+        console.log(this.gender)
+    }
+}
+
+class Person extends Human{
+    constructor() {
+        super();
+        this.name = "Colby";
+    }
+    printMyName() {
+        console.log(this.name);
+    }
+}
+
+const person  = new Person();
+person.printMyName();
+person.printGender();
