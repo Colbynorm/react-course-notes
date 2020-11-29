012a310f58be48d2c68aa73983612e6595ecbf4f
Colby Norman <colbynorm41@gmail.com>
2020-11-29 10:48:05 -0600

    added variable and arrow function notes

---
+++ chapter-2-JS/javaScriptNotes.js
@@ -0,0 +1,14 @@
+const myName = "Colby";
+console.log(myName);
+
+let myNewName = "Linnea";
+console.log(myNewName);
+
+//arrow functions
+function printMyName(name, age) {
+    console.log(name, age);
+}
+printMyName('Colby', 25);
+
+const multiply = number => number * 2;
+console.log(multiply(2));
