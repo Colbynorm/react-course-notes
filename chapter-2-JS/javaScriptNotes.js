ec7ab4ce35a248f9373cb433c67f36983e37e19d
Colby Norman <colbynorm41@gmail.com>
2020-12-06 15:13:30 -0600

    added spread/rest operators

--- chapter-2-JS/javaScriptNotes.js
+++ chapter-2-JS/javaScriptNotes.js
@@ -1,39 +1,57 @@
-//variable notes
-const myName = "Colby";
-console.log(myName);
+// //variable notes
+// const myName = "Colby";
+// console.log(myName);
 
-let myNewName = "Linnea";
-console.log(myNewName);
+// let myNewName = "Linnea";
+// console.log(myNewName);
 
-//arrow functions
-function printMyName(name, age) {
-    console.log(name, age);
-}
-printMyName('Colby', 25);
+// //arrow functions
+// function printMyName(name, age) {
+//     console.log(name, age);
+// }
+// printMyName('Colby', 25);
 
-const multiply = number => number * 2;
-console.log(multiply(2));
+// const multiply = number => number * 2;
+// console.log(multiply(2));
 
-//Classes
-class Human {
-    constructor() {
-        this.gender = "male";
-    }
-    printGender() {
-        console.log(this.gender)
-    }
-}
+// //Classes
+// class Human {
+//         gender = "male";
+//     printGender = () => {
+//         console.log(this.gender)
+//     }
+// }
 
-class Person extends Human{
-    constructor() {
-        super();
-        this.name = "Colby";
-    }
-    printMyName() {
-        console.log(this.name);
-    }
-}
+// class Person extends Human{
+//         name = "Colby";
+//         gender = "Male";
+//     }
+//     printMyName = () => {
+//         console.log(this.name);
+// }
 
-const person  = new Person();
-person.printMyName();
-person.printGender();
+// const person  = new Person();
+// person.printMyName();
+// person.printGender();
+
+
+// Spread & Rest Operators
+// const numbers = [1, 2, 3];
+// const newNumbers = [...numbers, 4];
+// console.log(newNumbers);
+
+// const person = {
+//     name: "Colby"
+// };
+// const newPerson = {
+//     ...person, 
+//     age: 25
+// };
+// console.log(newPerson);
+
+// const filter = (...args) => {
+//     return args.filter(el => el === 1);
+// }
+// console.log(filter(1, 2, 3));
+
+//Destructuring
