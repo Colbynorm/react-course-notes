1b80a885dacb18ebd688ab3db9a6d4b828e1ccaf
Colby Norman <colbynorm41@gmail.com>
2020-12-06 15:20:15 -0600

    added reference and primative type notes

--- chapter-2-JS/javaScriptNotes.js
+++ chapter-2-JS/javaScriptNotes.js
@@ -1,18 +1,18 @@
 // //variable notes
-// const myName = "Colby";
-// console.log(myName);
+const myName = "Colby";
+console.log(myName);
 
-// let myNewName = "Linnea";
-// console.log(myNewName);
+let myNewName = "Linnea";
+console.log(myNewName);
 
-// //arrow functions
-// function printMyName(name, age) {
-//     console.log(name, age);
-// }
-// printMyName('Colby', 25);
+//arrow functions
+function printMyName(name, age) {
+    console.log(name, age);
+}
+printMyName('Colby', 25);
 
-// const multiply = number => number * 2;
-// console.log(multiply(2));
+const multiply = number => number * 2;
+console.log(multiply(2));
 
 // //Classes
 // class Human {
@@ -55,3 +55,22 @@
 // console.log(filter(1, 2, 3));
 
 //Destructuring
+const numbers =[1, 2, 3];
+[num1, ,num3] = numbers;
+console.log(num1, num3);
+
+//Reference and Primative types
+//primative
+const number = 1;
+const num2 = number;
+console.log(num2);
+
+//reference
+const person = {
+    name: "Colby"
+};
+const secondPerson = {
+    ...person
+}
+person.name = "Linnea";
+console.log(secondPerson);
