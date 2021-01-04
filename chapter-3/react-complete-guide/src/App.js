98cd35849639109a7807db6167c906f6f525079e
Colby Norman <colbynorm41@gmail.com>
2021-01-03 20:22:31 -0600

    added changing state notes

--- chapter-3/react-complete-guide/src/App.js
+++ chapter-3/react-complete-guide/src/App.js
@@ -10,12 +10,25 @@ class App extends Component {
       { name: 'Frodo', age: 6 }
     ]
   }
+
+  switchNameHandler = () => {
+    //console.log('Was Clicked');
+    // Don't do this  ---  this.state.persons[0].name = 'Kyle';
+    this.setState({ 
+      persons: [
+        { name: 'Kyle', age: 26 },
+        { name: 'Alex', age: 26 },
+        { name: 'Hermon', age: 24 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi I am Colby!</h1>
         <p>Is this p tag working?</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobby is Coding</Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobby is Training</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
